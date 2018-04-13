84052af61dab90e7e88d2dc42175bdf6b930809e
Shazmin Mahmud <sm4423@columbia.edu>
2018-04-13 13:54:39 -0400

    Save changes on test_predict

---
+++ density/tests/test_predict.py
@@ -0,0 +1,34 @@
+import pytest
+import pandas as pd 
+
+from density import predict 
+from .predict import db_to_pandas, predict_tomorrow
+
+def test_predict(app):
+    resp = app.get('/predict')
+    assert resp.status_code == 200
+
+# test db_to_pandas
+# only need to test if the dataframe correctly converts to pd
+def test_db_to_pandas:
+	data = db_to_pandas(g.cursor)
+
+	# assert dataframe has correct number of rows (buildings)
+	assert len(data.index) == 23
+
+	# assert each row has correct number of columns
+	for index, row in data.iterrows():
+		assert len(row) == 6
+
+# test predict_tomorrow
+# need to test if the calculations for averages are correct
+def test_predict_tomorrow:
+	data = db_to_pandas(g.cursor)        # loading data from current database connection
+	predictions = predict_tomorrow(data) # get predictions
+
+	# assert dataframe is in correct format
+	for index, row in predictions.iterrows():
+		assert 
+
+	return 0;
+
