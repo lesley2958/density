6ee25217bee50d3533bf2e3bd3fe3063dd861ed3
Shazmin Mahmud <sm4423@columbia.edu>
2018-04-13 16:13:20 -0400

    Fix formatting errors

--- density/tests/test_predict.py
+++ density/tests/test_predict.py
@@ -1,7 +1,7 @@
-import pytest
 import pandas as pd 
+from flask import g,
 
-from density import predict 
+from density predict 
 from .predict import db_to_pandas, predict_tomorrow
 
 def test_predict(app):
@@ -11,25 +11,23 @@ def test_predict(app):
 # test db_to_pandas
 # only need to test if the dataframe correctly converts to pd
 def test_db_to_pandas():
-	data = db_to_pandas(g.cursor)
+    data = db_to_pandas(g.cursor)
 
-	# assert dataframe has correct number of rows (buildings)
-	assert len(data.index) == 23
+    # assert dataframe has correct number of rows (buildings)
+    assert len(data.index) == 23
 
-	# assert each row has correct number of columns
-	for index, row in data.iterrows():
-		assert len(row) == 6
+    # assert each row has correct number of columns
+    for _index, row in data.iterrows():
+        assert len(row) == 6
 
 # test predict_tomorrow
 # need to test if the calculations for averages are correct
-def test_predict_tomorrow():   
-	data = db_to_pandas(g.cursor)         # loading data from current database connection
-	predictions = predict_tomorrow(data)  # get predictions
+def test_predict_tomorrow():
+    data = db_to_pandas(g.cursor)         # loading data from current database connection
+    predictions = predict_tomorrow(data)  # get predictions
 
-	# assert dataframe is in correct format
-	for index, row in predictions.iterrows():
-		print(index, row)
+    # assert dataframe is in correct format
+    for index, row in predictions.iterrows():
+        print(index, row)
 
-	return 0;
-
-test_predict_tomorrow()
+    return 0;
